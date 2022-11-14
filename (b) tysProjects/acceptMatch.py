9acc3d1b68c58606c7e72529dbab3c47434874a9
Ty <tmitche5@uccs.edu>
2022-11-14 14:48:13 -0700

    Test 3

--- (b) tysProjects/acceptMatch.py
+++ (b) tysProjects/acceptMatch.py
@@ -1,18 +1,39 @@
+# League of Legends auto accept match script
 import time
 import pyautogui
 import win32api
 import win32con
+import pyautogui as pag
+pag.FAILSAFE = True
 
 
-#
-def click(x, y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+# This is the actual image recognition method
+def get_position(image: str):
+    try:
+        position = pag.locateCenterOnScreen(image, confidence=0.8)
+        if position is None:
+            return None
+        else:
+            x = position[0]
+            y = position[1]
+            return x, y
+    except OSError as e:
+        raise Exception(e)
 
 
-while pyautogui.pixel(520, 80)[0] == 34 or pyautogui.pixel(22, 11)[0] == 88:
-   
+# While the picture is the same as the league menu screen
+while __name__ == '__main__':
+    get_position('leagueScreen.png')
+
+    # This is the method to click the button
+    def click(x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+    # This says to click the button if it is the right color
     if pyautogui.pixel(897, 753)[0] == 30:
+
+        # This calls the click method
         click(955, 764)
