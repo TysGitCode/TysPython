8c427ec17849bf9063b61239236525bcaa186585
Ty <tmitche5@uccs.edu>
2022-11-16 13:45:04 -0700

    Test 3

---
+++ (b) tysProjects/acceptMatch.py
@@ -0,0 +1,40 @@
+# League of Legends auto accept match script
+import time
+import pyautogui
+import win32api
+import win32con
+import pyautogui as pag
+pag.FAILSAFE = True
+
+
+# This is the actual image recognition method
+def get_position(image: str):
+    try:
+        position = pag.locateCenterOnScreen(image, confidence=0.8)
+        if position is None:
+            print(f'{image} not found on screen...')
+            return None
+        else:
+            x = position[0]
+            y = position[1]
+            return x, y
+    except OSError as e:
+        raise Exception(e)
+
+
+# While the picture is the same as the league menu screen
+while __name__ == '__main__':
+    get_position('leagueScreen.png')
+
+    # This is the method to click the button
+    def click(x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+    # This says to click the button if it is the right color
+    if pyautogui.pixel(897, 753)[0] == 30:
+
+        # This calls the click method
+        click(955, 764)
--- (b) tysProjects/openWindowed.vbs
+++
@@ -1,3 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "C:\Users\xsnot\git\TysPython\(b) tysProjects\leagueAccept\acceptMatch.py" & Chr(34), 0
-Set WshShell = Nothing
