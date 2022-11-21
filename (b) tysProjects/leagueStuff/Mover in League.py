ab9b02c17664206bb5bf8b60375f0c13a1b1c4b9
Ty <tmitche5@uccs.edu>
2022-11-20 22:01:39 -0700

    Test 3

---
+++ (b) tysProjects/leagueStuff/Mover in League.py
@@ -0,0 +1,21 @@
+import time
+import pyautogui
+import win32api
+import win32con
+import pyautogui as pag
+
+pag.FAILSAFE = True
+
+
+while 1 == 1:
+    # This is the method to click the button
+    def click(x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+    click(955, 764)
+    time.sleep(5)
+    click(975, 764)
+    time.sleep(5)
