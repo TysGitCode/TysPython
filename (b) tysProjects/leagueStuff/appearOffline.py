341f5b6026813586dbafc49074f568f7d52b7088
Ty <tmitche5@uccs.edu>
2022-11-20 22:14:46 -0700

    Test 3

--- (b) tysProjects/leagueStuff/appearOffline.py
+++ (b) tysProjects/leagueStuff/appearOffline.py
@@ -1,4 +1,15 @@
 import os
+import keyboard
+
+import button as button
+import timer
 
 os.system('cmd /k "netsh advfirewall firewall add rule name=”lolchat” dir=out remoteip=192.64.174.69 protocol=TCP '
           'action=block"')
+
+exit()
+
+
+
+
+
